1e7db9760e6aaf00e07ae3f78dff7c96c6c28633
Kamil Brzozowski <kamil.brzozowski@miquido.com>
2021-11-24 21:10:15 +0100

    add changes to nav

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,27 @@
-import { Container } from './style'
+import { Container, NavigationContainer, NavigationGroup, SingleNavigation, SingleNavigationActive, ButtonCreateReview } from './style'
+import { faBuilding, faStickyNote } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Navbar = () => (
-    <Container>
-        <p>Staże Dla Studentów</p>
-    </Container>
+    <div>
+        <Container>
+            <p>Staże Dla Studentów</p>
+        </Container>
+        <NavigationContainer>
+            <NavigationGroup>
+                <SingleNavigationActive>
+                    <FontAwesomeIcon icon={faBuilding}/>
+                    <p>Firmy</p>
+                </SingleNavigationActive>
+
+                <SingleNavigation>
+                    <FontAwesomeIcon icon={faStickyNote}/>
+                    <p>Pytania Rekrutacyjne</p>
+                </SingleNavigation>
+
+            </NavigationGroup>
+            <ButtonCreateReview>Stwórz nową recenzje</ButtonCreateReview>
+        </NavigationContainer>
+    </div>
+
 )
--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -14,3 +14,61 @@ export const Container = styled.div`
         color: #FFFFFF;
     }
 `
+
+export const NavigationContainer = styled.div`
+    width: 100%;
+    height: .5rem;
+    background-color: #283845;
+    padding: 2rem 0;
+    border-bottom: 1px solid #000;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+`
+
+export const NavigationGroup = styled.div`
+    display: flex;
+    align-items: center;
+
+`
+
+export const SingleNavigation = styled.div`
+    display: flex;
+    align-items: center;
+    color: #FFFFFF;
+    font-size: 1.4rem;
+    margin: 0 1rem;
+    cursor: pointer;
+
+    p {
+        margin-left: .5rem;
+    }
+
+`
+
+export const SingleNavigationActive = styled.div`
+    display: flex;
+    align-items: center;
+    color: #FFFFFF;
+    font-size: 1.4rem;
+    margin: 0 1rem;
+    cursor: pointer;
+
+    p {
+        margin-left: .5rem;
+    }
+    border-bottom: 1px solid #58cae0;
+`
+
+export const ButtonCreateReview = styled.button`
+    background-color: #202C39;
+    color: #fff;
+    text-decoration: none;
+    display: inline-block;
+    padding: .6rem 1rem;
+    border: none;
+    font-size: 1rem;
+    cursor: pointer;
+    border: 1px solid #000;
+`
