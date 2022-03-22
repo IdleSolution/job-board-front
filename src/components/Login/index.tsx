e8bf0fffd4d827322d037ea8fadd44e1f7d37fbf
kamil.brzozowski <kamil.brzozowski@miquido.com>
2022-03-22 12:27:57 +0100

    Fix login form

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,19 +1,36 @@
 import axios from "axios";
+import {useState} from "react";
 
 export const Login = () => {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
     const onLogin = async (e: any) => {
         e.preventDefault();
+        console.log(login);
+        console.log(password);
         const res = await axios.post('http://localhost:5000/api/User/login', {
-            email: 'idlesoluszyn@gmail.com',
-            password: 'CBDUM123!@#yt'
+            email: login,
+            password: password
         }, {withCredentials: true})
         console.log(res);
         console.log('logged in...')
     }
+
+    const onLoginChange = (e: any) => {
+        const { value } = e.target;
+
+        setLogin(value);
+    }
+
+    const onPasswordChange = (e: any) => {
+        const { value } = e.target;
+
+        setPassword(value);
+    }
     return (
         <form onSubmit={(e) => onLogin(e)}>
-            <input />
-            <input />
+            <input placeholder='Email' onChange={(e: any) => onLoginChange(e)}/>
+            <input type='password' placeholder='Password' onChange={(e: any) => onPasswordChange(e)} />
             <button>Zaloguj</button>
         </form>
     )
